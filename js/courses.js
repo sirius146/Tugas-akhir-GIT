5a484d746fd3d79b402d63bb7b6c54861b26beb8
sirius146 <dwiatika479@yahoo.com>
2021-04-09 10:46:56 +0700

    first commit

---
+++ js/courses.js
@@ -0,0 +1,13 @@
+var courses = [{
+    name: 'Full Stack Immersive',
+    details: 'Pada program Full Stack JavaScript terdiri dari fase Preparation sampai dengan Bootcamp yang terdiri dari 3 Fase',
+    picture: 'https://global-uploads.webflow.com/5f0d53c042a9ed6288de7f8d/5fd31fc288a2a7375b87543b_1%20(26).jpg'
+}, {
+    name: 'Full Stack Flex',
+    details: 'Pada program Full Stack JavaScript terdiri dari fase Preparation sampai dengan Bootcamp yang terdiri dari 3 Fase',
+    picture: 'https://global-uploads.webflow.com/5f0d53c042a9ed6288de7f8d/5fd31fdad928ad04ca15c5b1_9%20(2).jpg'
+}, {
+    name: 'Part Time Program',
+    details: 'Waktu pembelajaran akan dilakukan di luar jam kerja selama kurang dari 4 bulan dalam 2-3 kali seminggu, pukul 07.00 - 10.00 malam.',
+    picture: 'https://global-uploads.webflow.com/5f0d53c042a9ed6288de7f8d/5fd31fe1c6e4d856269a8269_7%20(2).jpg'
+}]
---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+//Loading Functions
+var myVar;
+function showPage() {
+  $('#loading').css('display', 'none');
+  $('#page').css('display', 'block');
+}
+function loadPage() {
+  myVar = setTimeout(showPage, 1000);
+}
+
+
+//Local Storage Function
+function onsubmitForm() {
+  if (typeof(Storage) !== 'undefined') {
+    var email = $('#emailAddress').val();
+    localStorage.setItem('email', email);
+
+  }
+  else {
+    alert('Maaf, browser anda tidak support local storage');
+  }
+}
+
+courses.forEach(function(course){
+  $('#program-courses').append('<div class="col-md-4"><div class="card"><img src="'
+  + course.picture + '" class="card-img-top" alt="'
+  + course.name + '"><div class="card-body"><h5 class="card-title">'
+  + course.name + '</h5><p class="card-text">'
+  + course.details + '</p></div></div></div>');
+});
